e09c636813dec8a4145a572557f566f7fb11cdd8
mforjan <mforjan@andrew.cmu.edu>
2018-03-02 11:30:07 -0800

    added app.js and env.sh

---
+++ app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// Require everything
+var fs = require('fs');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// Initialize express
+var app = express();
+
+var MONGODB_URI = MONGODB_URI_1 || MONGODB_URI_2 || MONGODB_URI_3;
+
+// Check for env.sh file
+if (! fs.existsSync('./env.sh')) {
+  throw new Error('env.sh file is missing');
+}
+if (! process.env.MONGODB_URI) {
+  throw new Error("MONGODB_URI is not in the environmental variables. Try running 'source env.sh'");
+}
+
+// Test MongoDB connection
+mongoose.connection.on('connected', function() {
+  console.log('Success: connected to MongoDb!');
+});
+mongoose.connection.on('error', function() {
+  console.log('Error connecting to MongoDb. Check MONGODB_URI in env.sh');
+  process.exit(1);
+});
+mongoose.connect(process.env.MONGODB_URI);
